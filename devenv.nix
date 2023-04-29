dcf000682206b4d1a91cb0909072b2c3fa4d2b39
tomasmota <tomasrebelomota@gmail.com>
2023-04-29 21:15:38 +0200

    add devenv file
    Signed-off-by: tomasmota <tomasrebelomota@gmail.com>

--- devenv.nix
+++ devenv.nix
@@ -5,27 +5,27 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git
+  packages = [ 
+    pkgs.git
     pkgs.nodejs-19_x 
     pkgs.zsh
+    pkgs.yarn
   ];
 
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
 
-  enterShell = ''
-    hello
-    git --version
-  '';
+  # enterShell = ''
+  #   zsh
+  # '';
 
   # https://devenv.sh/languages/
-  # languages.nix.enable = true;
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
+  languages.typescript.enable = true;
 
   # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
+  processes = {
+    pulsar.exec = "podman run -it -p 6650:6650 -p 8080:8080 --mount type=volume,source=pulsardata,target=/pulsar/data --mount type=volume,source=pulsarconf,target=/pulsar/conf apachepulsar/pulsar:2.11.0 bin/pulsar standalone";
+    fill-topic.exec = "tsc plugins/pulsar/scripts/fill-topic.ts && node plugins/pulsar/scripts/fill-topic.js";
+    backstage.exec = "yarn dev";
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
