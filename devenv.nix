a3611ff13cfd5ec6f2611d7dc195c44695e8a9a0
tomasmota <tomasrebelomota@gmail.com>
2023-04-30 07:52:03 +0200

    devenv
    Signed-off-by: tomasmota <tomasrebelomota@gmail.com>

--- devenv.nix
+++ devenv.nix
@@ -1,31 +1,16 @@
 { pkgs, ... }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
-  # https://devenv.sh/packages/
   packages = [ 
-    pkgs.git
-    pkgs.nodejs-19_x 
-    pkgs.zsh
+    pkgs.nodejs-18_x 
     pkgs.yarn
+    pkgs.typescript
+    pkgs.podman
   ];
 
-
-  # enterShell = ''
-  #   zsh
-  # '';
-
-  # https://devenv.sh/languages/
-  languages.typescript.enable = true;
-
-  # https://devenv.sh/processes/
   processes = {
     pulsar.exec = "podman run -it -p 6650:6650 -p 8080:8080 --mount type=volume,source=pulsardata,target=/pulsar/data --mount type=volume,source=pulsarconf,target=/pulsar/conf apachepulsar/pulsar:2.11.0 bin/pulsar standalone";
     fill-topic.exec = "tsc plugins/pulsar/scripts/fill-topic.ts && node plugins/pulsar/scripts/fill-topic.js";
     backstage.exec = "yarn dev";
   };
-
-  # See full reference at https://devenv.sh/reference/options/
 }
