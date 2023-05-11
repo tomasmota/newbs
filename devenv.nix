956f73c520a30bb1cce2426f72261196ecf1a9ed
Tomás Mota <tmm@danskecommodities.com>
2023-05-11 07:46:42 +0200

    sleep before topic filling
    Signed-off-by: Tomás Mota <tmm@danskecommodities.com>

--- devenv.nix
+++ devenv.nix
@@ -14,6 +14,6 @@
 
   processes = {
     pulsar.exec = "docker run -p 6650:6650 -p 8080:8080 --mount type=volume,source=pulsardata,target=/pulsar/data --mount type=volume,source=pulsarconf,target=/pulsar/conf apachepulsar/pulsar:2.11.0 bin/pulsar standalone";
-    fill-topic.exec = "tsc plugins/pulsar/scripts/fill-topic.ts && node plugins/pulsar/scripts/fill-topic.js";
+    fill-topic.exec = "sleep 5 && tsc plugins/pulsar/scripts/fill-topic.ts && node plugins/pulsar/scripts/fill-topic.js";
   };
 }
