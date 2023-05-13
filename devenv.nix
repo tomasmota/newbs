844aa3cb3cb4cc224e369c2a929311d6ce3d5c8d
tomasmota <tomasrebelomota@gmail.com>
2023-05-13 16:57:17 +0200

    fix devenv
    Signed-off-by: tomasmota <tomasrebelomota@gmail.com>

--- devenv.nix
+++ devenv.nix
@@ -1,19 +1,34 @@
 { pkgs, ... }:
 
 {
-  packages = [ 
-    pkgs.nodejs-18_x 
-    pkgs.yarn
-    pkgs.typescript
-    pkgs.podman
-    pkgs.docker
-    pkgs.git
-  ];
+    packages = [ 
+        pkgs.nodejs-18_x 
+        pkgs.yarn
+        pkgs.typescript
+        pkgs.git
+        pkgs.podman 
+    ];
 
-  scripts.dev.exec = "yarn dev";
+    scripts = {
+        dev.exec = "yarn dev";
+        pulsar.exec = ''
+            if podman ps &> /dev/null; then
+                cli="podman"
+            elif docker ps &> /dev/null; then
+                cli="docker"
+            else
+                echo "no container tool installed, exiting"
+                exit 1
+            fi
 
-  processes = {
-    pulsar.exec = "docker run -p 6650:6650 -p 8080:8080 --mount type=volume,source=pulsardata,target=/pulsar/data --mount type=volume,source=pulsarconf,target=/pulsar/conf apachepulsar/pulsar:2.11.0 bin/pulsar standalone";
-    fill-topic.exec = "sleep 5 && tsc plugins/pulsar/scripts/fill-topic.ts && node plugins/pulsar/scripts/fill-topic.js";
-  };
+            $cli run \
+                -p 6650:6650 \
+                -p 8080:8080 \
+                --mount type=volume,source=pulsardata,target=/pulsar/data \
+                --mount type=volume,source=pulsarconf,target=/pulsar/conf \
+                apachepulsar/pulsar:2.11.0 bin/pulsar standalone
+        '';
+
+        fill.exec = "sleep 5 && tsc plugins/pulsar/scripts/fill-topic.ts && node plugins/pulsar/scripts/fill-topic.js";
+    };
 }
